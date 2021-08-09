1ced3483a93e6d1dd2cab02b805e65c40ca46e3e
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-09 17:06:08 +0200

    home container created

---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+
+    return(
+    
+        <div>
+            SOY HOME
+        </div>
+    )
+
+}
+
+export default Home;
