e637bbcc0c9e28f13046c89324f0df09016cf7fe
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-09 17:06:17 +0200

    about me  container created

---
+++ src/Containers/AboutMe/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AboutMe.css'
+
+const AboutMe = () => {
+
+    return(
+    
+        <div>
+            SOY AboutMe
+        </div>
+    )
+
+}
+
+export default AboutMe;
