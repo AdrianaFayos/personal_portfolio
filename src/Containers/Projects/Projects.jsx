a6ca7b43826282e4055ffa4d6c8d0afb74098cdc
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-09 17:06:32 +0200

    projects  container created

---
+++ src/Containers/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Projects.css'
+
+const Projects = () => {
+
+    return(
+    
+        <div>
+            SOY Projects
+        </div>
+    )
+
+}
+
+export default Projects;
